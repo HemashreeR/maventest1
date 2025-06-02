e7eb9458725a16a833f47e8553d981c05ef8de7f
Hemashree <160819289+HemashreeR@users.noreply.github.com>
2025-06-02 09:12:42 +0530

    Create helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,13 @@
+package com.p4.maven.exp4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello hemashreeee!" );
+    }
+}
